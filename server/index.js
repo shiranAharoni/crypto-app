5719ac5384cda332cf726ca73c94df98a0771a53
shiranAharoni <shiran5725@gmail.com>
2026-01-21 10:46:05 +0200

    Update DB connection for production with SSL

--- server/index.js
+++ server/index.js
@@ -15,11 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 app.post("/register", async (req,res) =>{
